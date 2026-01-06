26a8f34f9157ed80c8206b61d13091103bd692f4
ernyweb <ernyzasxash@proton.me>
2026-01-06 19:13:45 +0000

    Build: APK derlemesi tamamlandı - Debug (5.4M) ve Release (2.0M)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,16 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     kotlin("android") version "1.9.0" apply false
+    kotlin("jvm") version "1.9.0" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(layout.buildDirectory)
 }
