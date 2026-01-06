abdfd121c29ca275bcfa962f6d9d97622380efd1
ernyweb <ernyzasxash@proton.me>
2026-01-06 14:24:17 +0000

    Fix iOS entry point: Add UIApplication delegate with main() function

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,42 @@ void android_main(struct android_app* state) {
 }
 
 #elif defined(PLATFORM_IOS)
-// iOS entry point would be in Objective-C/Swift wrapper
+// iOS entry point - minimal UIApplication setup
+#include <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic, strong) UIWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor blackColor];
+    
+    UIViewController *viewController = [[UIViewController alloc] init];
+    self.window.rootViewController = viewController;
+    [self.window makeKeyAndVisible];
+    
+    // Initialize and run game in a background thread to avoid blocking UI
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        static Game game;
+        if (game.Initialize()) {
+            game.Run();
+        }
+        game.Shutdown();
+    });
+    
+    return YES;
+}
+
+@end
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
 
 #else
 // Desktop entry point for testing
