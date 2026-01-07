7a637970378f2192d495c81164fff2eb4cb91e03
ernyweb <ernyzasxash@proton.me>
2026-01-07 08:15:19 +0000

    sfasdf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,13 +10,23 @@ android {
         applicationId = "com.gamebooster.launcher"
         minSdk = 24
         targetSdk = 33
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "3.0.0"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
         }
         debug {
         }
